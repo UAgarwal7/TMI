6f49cda7aa67fdef7d075c90edc4181ac8b40afc
Utsav Agarwal <utsavagarwal@Sachins-MacBook-Pro.local>
2021-11-28 16:24:52 -0800

    testing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return "testing"
 #    return render_template("templates/home.html")
 
